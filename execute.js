404c424552f8cda84f16b26589f71e7dd8f44c61
Ryo Ota <nwtgck@gmail.com>
2017-10-01 19:18:06 +0900

    [refactor] Refactor goToInitialCommit function to use async function

--- execute.js
+++ execute.js
@@ -4,7 +4,7 @@
 goToInitialCommit();
 
 // (hint from https://github.com/FarhadG/init/blob/master/src/index.js)
-function goToInitialCommit(){
+async function goToInitialCommit(){
   // The element of the number of commits
   const nCommitsElm = document.querySelector('.num.text-emphasized');
 
@@ -12,31 +12,21 @@ function goToInitialCommit(){
     // The number of commits
     const nCommits = parseInt(nCommitsElm.innerText);
 
-    fetch(`https://api.github.com/repos${window.location.pathname}/commits`)
+    const res = await fetch(`https://api.github.com/repos${window.location.pathname}/commits`)
+    
+    const results = await Promise.all([res.headers.get('link'), res.json()])
 
-      .then(res => {
-        console.log(res);
-        console.log(res.headers.get('link'));
-        return Promise.all([res.headers.get('link'), res.json()])
-      })
+    // Get commits
+    const commits = results[1];
 
-      .then(results => {
-        // Get commits
-        return results[1];
-      })
+    // Get latest commit (not initial commit)
+    const latestCommit = commits[0];
 
-      .then(commits => {
-        // Get latest commit (not initial commit)
-        return commits[0];
-      })
+    // Get sha
+    const sha = latestCommit.sha;
+    // Go to initial commit page
+    window.location = `https://github.com${window.location.pathname}/commits/master?after=${sha}+${nCommits-2}`
 
-      // navigate there
-      .then(latestCommit => {
-        // Get sha
-        const sha = latestCommit.sha;
-        // Go to initial commit page
-        window.location = `https://github.com${window.location.pathname}/commits/master?after=${sha}+${nCommits-2}`
-      });
 
   } else {
     alert("Go repository top page!")
